b6c621147e89778f0a3a4160abcb3e9b59d2d056
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-02 17:20:28 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,7 +10,9 @@ class SongsController < ApplicationController
   end
 
   def create
-    
+    @song = Song.new(params.require(:song))
+    @song.save 
+    redirect_to song_path(@song)
   end
 
   def edit
