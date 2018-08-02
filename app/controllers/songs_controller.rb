526e3b4c861426cd43c460a37ae26134a52ef044
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-02 17:52:01 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,6 +20,9 @@ class SongsController < ApplicationController
   end
 
   def update
+    @song = Song.find[params[:id]]
+    @song.update(song_params)
+    redirect_to song_path(@song)
   end
 
 end
