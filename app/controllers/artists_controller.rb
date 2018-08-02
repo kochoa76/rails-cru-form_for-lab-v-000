d03b82ac3d4f6966c0224d0bac33fbb5d46ed68c
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-02 17:39:12 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -21,8 +21,14 @@ class ArtistsController < ApplicationController
 
     def update
       @artist = Artist.find(params[:id])
-      @artist.update(params.require(:artist).permit(:name, :bio))
+      @artist.update(artist_params)
       @artist.save
       redirect_to artist_path(@artist)
     end
+
+    private 
+
+    def artist_params
+			params.require(:artist).permit(:name, :bio)
+      
 end
