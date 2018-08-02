e4d49c146cd1c70f6bff475f882e4eb7c2a33c06
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-02 17:22:06 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,7 +9,7 @@ class ArtistsController < ApplicationController
   end
 
     def create
-      @artist = Artist.new(params.require[:artist])
+      @artist = Artist.new(params.require(:artist))
       @artist.save
       redirect_to artist_path(@artist)
       
